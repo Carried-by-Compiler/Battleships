de9c28866ef92c633ca21db5e2a9c81f68e3673c
HenryMaugh <15141608@studentmail.ul.ie>
2017-04-26 17:08:57 +0100

    Vhas both scoring and databse in it

---
+++ V4/app/src/main/java/com/example/battleships/ListDataActivity.java
@@ -0,0 +1,53 @@
+package com.example.battleships;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hmaug on 03/04/2017.
+ */
+
+public class ListDataActivity  extends AppCompatActivity {
+
+    private static final String TAG = "ListDataActivity";
+
+    DatabaseHelper mDatabaseHelper;
+
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscores);
+        mListView = (ListView) findViewById(R.id.ListView);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        populateListView();
+    }
+
+    private void populateListView() {
+        Log.d(TAG, "populateListView: Displaying data in the ListView.");
+
+        Cursor data = mDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList();
+        while(data.moveToNext()){
+            listData.add(data.getString(1));
+        }
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listData);
+        mListView.setAdapter(adapter);
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
---
+++ V4/app/src/main/java/com/example/battleships/ViewBoats.java
@@ -0,0 +1,92 @@
+package com.example.battleships;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by johnr on 26/04/2017.
+ */
+
+public class ViewBoats extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_turn);
+
+        new DisplayBoats().execute();
+        Button fireButton = (Button)findViewById(R.id.fireButton);
+        fireButton.setVisibility(View.INVISIBLE);
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setText("Score:");
+        TextView posField = (TextView)findViewById(R.id.posField);
+        posField.setText(String.valueOf(PVP.game.getScore()));
+        TextView hitMarker = (TextView)findViewById(R.id.hit_marker);
+        hitMarker.setVisibility(View.INVISIBLE);
+        TextView status = (TextView)findViewById(R.id.status_message);
+        hitMarker.setVisibility(View.INVISIBLE);
+        Button viewBoats = (Button)findViewById(R.id.view_boats);
+        viewBoats.setVisibility(View.VISIBLE);
+        viewBoats.setText("Go Back");
+        viewBoats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewBoats.this.finish();
+            }
+        });
+    }
+
+    private int getStringIdentifier(Context pContext, String pString) {
+        return pContext.getResources().getIdentifier(pString, "id", pContext.getPackageName());
+    }
+
+    private class DisplayBoats extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            for (Boat b: PVP.game.getBoats()) {
+                for (Point p: b.getPoints()) {
+                    String stringId = p.getCoordinate();
+                    int identifier = getStringIdentifier(ViewBoats.this, stringId);
+                    publishProgress(identifier, 0);
+                }
+            }
+
+            for (Point p: PVP.hitPoints) {
+                String stringId = p.getCoordinate();
+                int identifier = getStringIdentifier(ViewBoats.this, stringId);
+                publishProgress(identifier, 1);
+            }
+
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Button button;
+            button = (Button)findViewById(values[0]);
+
+            if (values[1] == 1)
+                button.setBackgroundResource(R.drawable.fire);
+            else
+                button.setBackgroundResource(R.drawable.silent);
+        }
+    }
+}
