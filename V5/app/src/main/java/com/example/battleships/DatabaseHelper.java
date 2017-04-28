d6b5611f2e0e23c6b2ddbe1c841c21e2a5af80e4
Aaron Eaton Jones <15168786@studentmail.ul.ie>
2017-04-28 23:45:58 +0000

    Removed the sounds and fixed leaderboard background

---
+++ V5/app/src/main/java/com/example/battleships/DatabaseHelper.java
@@ -0,0 +1,63 @@
+package com.example.battleships;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by hmaug on 03/04/2017.
+ */
+
+public class DatabaseHelper  extends SQLiteOpenHelper{
+
+    private static  final String TAG = "DatabaseHelper";
+
+    private static final String TABLE_NAME = " people_table";
+    private static  final String COL1 = "Score";
+    private static  final String COL2 = "name";
+
+    public DatabaseHelper(Context context){
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String createTable = "CREATE TABLE " + TABLE_NAME + "Score INTERGER PRIMARY KEY AUTOINCRIMENT, " + " name TEXT) " ;
+        db.execSQL(createTable);
+        db.execSQL("INSERT INTO " + TABLE_NAME + "(Score) VALUE(1000)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1){
+        db.execSQL("DROP IF TABLE EXISTS" + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public  boolean addData(String item){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, item);
+
+        Log.d(TAG, "addData : Adding. " + item + " to " + TABLE_NAME);
+
+        Long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM" + TABLE_NAME ;
+        Cursor data = db.rawQuery(query, null);
+        return data ;
+    }
+
+}
---
+++ V5/app/src/main/java/com/example/battleships/menu.java
@@ -0,0 +1,126 @@
+package com.example.battleships;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.MutableInt;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class menu extends AppCompatActivity {
+
+    DatabaseHelper mDatabaseHelper;
+    private Button  highscoresButton;
+    private boolean MusicRunning = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        highscoresButton = (Button) findViewById((R.id.highscoresButton));
+        mDatabaseHelper = new DatabaseHelper(this);
+
+
+        highscoresButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menu.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setButtonOnClickListeners(); //  initialises the button listeners.
+    }
+
+    private  void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setButtonOnClickListeners(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(menu.this); // creates alert dialog.
+                builder.setMessage("Do You want automated ship placement?");
+                builder.setCancelable(true);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        /*dialog.cancel();*/
+                        Intent intent = new Intent(menu.this, deployMenu.class);
+                        intent.putExtra("DEPLOY_MESSAGE", 1);
+                        intent.putExtra("MULTIPLAYER", 0);
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(menu.this, deployMenu.class);
+                        intent.putExtra("DEPLOY_MESSAGE", 0);
+                        finish(); // Ends activity.
+                        startActivity(intent); // starts intent.
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        };
+        findViewById(R.id.pveButton).setOnClickListener(listener); // initialises listener.
+
+        View.OnClickListener listener2 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                Intent intent = new Intent(menu.this, pvpMenu.class); // initialises intent.
+                finish(); // Ends activity.
+                startActivity(intent); // starts intent.
+            }
+        };
+        findViewById(R.id.pvpButton).setOnClickListener(listener2); // initialises listener.
+
+        View.OnClickListener listener3 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                Intent intent = new Intent(menu.this, leaderBoard.class); // initialises intent.
+                finish(); // ends activity.
+                startActivity(intent); // starts intent.
+            }
+        };
+        findViewById(R.id.highscoresButton).setOnClickListener(listener3); // initialises listener.
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(menu.this); // creates alert dialog.
+        builder.setMessage("Do you want to exit?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                BluetoothAdapter b = BluetoothAdapter.getDefaultAdapter();
+                b.disable();
+                finishAffinity(); // Ends application
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
+
+
